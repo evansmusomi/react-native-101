f2fca7162906b13ff7af46260460797de88f81af
Evans Musomi <evans.musomi@gmail.com>
2018-05-12 05:56:47 +0000

    configures project to run expo app from c9 workspace

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Text>Mama! I finally made it!</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
