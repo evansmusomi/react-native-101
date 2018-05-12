639af304a9a5818bd27e3bb0dd077e5df560bc60
Evans Musomi <evans.musomi@gmail.com>
2018-05-12 07:12:35 +0000

    destructures react component

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
         <Text>Mama! I finally made it!</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
